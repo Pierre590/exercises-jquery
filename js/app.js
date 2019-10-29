4bc172df2df239f76ca49a6f45721860f2af160a
pierre berengue <nanashi4781@gmail.com>
2019-10-29 10:57:13 +0100

    finexo3

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 $(function() {
-  console.log($("#navbarDropdown"))
+  
 
   $("#navbarDropdown").click(function(){
    $(".dropdown-menu").toggle(); 
   })
   
   $(".nav-link.disabled").removeClass("disabled")
+
+
+  $("#search").keyup(function(){
+    // valeur du champ
+    var value = $("#search").val();
+    $("#searchResult").html(value)
+  })
+
+
+
+
+
+
+
 });
