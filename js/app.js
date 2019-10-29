3413f24c7c63100146e2c2ac1b43050a24ffd90f
pierre berengue <nanashi4781@gmail.com>
2019-10-29 16:03:28 +0100

    finish exo

--- js/app.js
+++ js/app.js
@@ -28,8 +28,16 @@ $(function() {
   $("#playVideo").click(function(){
     $("#video").get(0).play(); 
       setTimeout(function(){("#video")},10000);
+// ou var video =$('video').get(0);
+//  video.play();
+//  setTimeout(function(){
+  // video.pause();
+  // 10000);}
+
+
   });
 //ce que je souhaite faire dans mon cv
+
   $('#myTab a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
@@ -43,6 +51,36 @@ $(".close,#close").click(function(){
 });
 
 $("#popover").popover();
-
 });
+//exo 10
+var progressBar = $("#progressBar");
+
+  setInterval(function () {
+    var width = parseInt(progressBar[0].style.getPropertyValue('width'))
+
+      if (width >= 100) {
+      progressBar.css('width', 0)
+    } else {
+      progressBar.css('width', '100%') //width + 5 + '%'
+    }
+  }, 1000)
+
+  // exo11
+
+  var isPlayed = true;
+
+  setInterval(function() {
+    if (isPlayed) {
+      $('#spinner').css('animation-play-state','paused');
+      isPlayed = false;
+    }else {
+        $('#spinner').css('animation-play-state','running');
+        isPlayed = true; 
+      }
+    },5000)
+
+  
+
+
+
 
